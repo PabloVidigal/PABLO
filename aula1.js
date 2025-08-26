c26e179facc145f421b8891a01438a744aa525ec
Pablo Rodrigues Vidigal <Pablorodriguesvidigal@gmail.com>
2025-08-26 20:44:00 -0300

    Initial commit

---
+++ aula1.js
@@ -0,0 +1,18 @@
+
+console.log(2+4);
+console.log(4*2);
+console.log(5/2);
+console.log(5%2);
+console.log(5-3);
+console.log(5/0);
+console.log(typeof `maria`);
+console.log(typeof 'maria');
+console.log(typeof 13);
+console.log(typeof "13");
+console.log(9!="9");
+console.log(5>3&&9!="9");
+
+console.log(Math.sqrt(9));
+console.log(Math.PI);
+console.log(Math.random());
+console.log(Math.pow(3,2));
