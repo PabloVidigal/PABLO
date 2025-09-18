6193543c6a9ebab029e90394a79a8056a3b9e2b5
Pablo Rodrigues Vidigal <Pablorodriguesvidigal@gmail.com>
2025-09-18 20:12:38 -0300

    a

--- lista08.js
+++ lista08.js
@@ -1,4 +1,5 @@
 //EXERCICIO 01
+/*
 function verificarNumero(numero){
 
     let verificar = Math.floor(numero /100000);
@@ -26,4 +27,100 @@ if (verificar === 3225) {
 
 const numeroDeTelefone = 32121212;
 console.log("O numero citado pertence ao bairro: " + verificarNumero(numeroDeTelefone))
+*/
 
+// EXERCICIO 02
+
+function verificarPalindromo(numero) {
+    if (numero < 10000 || numero > 99999) {
+        console.log("O número precisa ter exatamente 5 dígitos.");
+        return;
+    }
+
+    let digito1 = Math.floor(numero / 10000);         
+    let digito2 = Math.floor((numero % 10000) / 1000); 
+    let digito4 = Math.floor((numero % 100) / 10);    
+    let digito5 = numero % 10;                         
+
+ 
+    if (digito1 === digito5 && digito2 === digito4) {
+        console.log("O número " + numero+" é palíndromo.");
+    } else {
+        console.log("O número " + numero+" não é palíndromo.");
+    }
+}
+const entrada = 12321;
+verificarPalindromo(entrada);
+
+// EXERCICIO 03
+
+function testeTriangulo(A,B,C){
+    if(A* A ==B*B+C*C){
+        return "Retângulo"
+    }else if(A*A>B*B+C*C){
+       return "Obtusângulo"
+    }else if(A*A<B*B+C*C){
+        return "Acutângulo"
+    }
+}
+const a = 3;
+const b = 3;
+const c = 3;
+console.log(testeTriangulo(a,b,c))
+
+// EXERCICIO 04
+
+function somaTresMenores(a, b, c, d) {
+    let maior = a;
+    if (b > maior) {
+        maior = b;
+    }
+    if (c > maior) {
+        maior = c;
+    }
+    if (d > maior) {
+        maior = d;
+    }
+
+    let somaTotal = a + b + c + d;
+
+    return somaTotal - maior;
+}
+
+const An = 1;
+const Bn = 2;
+const Cn = 3;
+const Dn = 4;
+console.log(somaTresMenores(An,Bn,Cn,Dn))
+
+// EXERCICIO 05
+
+function calcularValorFinal(corEtiqueta, quantidade, valorUnitario) {
+    let desconto = 0;
+
+    if (corEtiqueta.toLowerCase() === "verde") {
+        if (quantidade <= 10) {
+            desconto = 0.05; 
+        } else {
+            desconto = 0.10;
+        }
+    } else if (corEtiqueta.toLowerCase() === "amarelo") {
+        if (quantidade <= 5) {
+            desconto = 0.15; 
+        } else {
+            desconto = 0.20; 
+        }
+    } else {
+        
+        return "Cor da etiqueta inválida.";
+    }
+
+    let valorTotal = quantidade * valorUnitario;
+    let valorFinal = valorTotal - (valorTotal * desconto);
+
+   return"Valor final a pagar: " + valorFinal;
+}
+const cor = "Verde";
+const produtos = 8;
+const valorCada = 100;
+console.log(calcularValorFinal(cor,produtos,valorCada))
